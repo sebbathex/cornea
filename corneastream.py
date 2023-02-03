4be280d87c62afadba87bfe7cc3de2eb02b611e6
sebbathex <106611693+sebbathex@users.noreply.github.com>
2023-02-03 07:55:34 +0100

    Update corneastream.py

--- corneastream.py
+++ corneastream.py
@@ -11,6 +11,7 @@ from sklearn.naive_bayes import CategoricalNB
 from sklearn.neural_network import MLPClassifier
 from sklearn import svm
 from collections import Counter
+import boto3
 
 
 
@@ -35,6 +36,19 @@ def write_navigation_bar():
 
 
 def write_main_page():
+    
+        # Create a client object for the S3 service
+    s3 = boto3.client("s3")
+
+    # Create a bucket if it doesn't already exist
+    bucket_name = "sebbax"
+    s3.create_bucket(Bucket=bucket_name)
+
+    # Write text to a file in S3
+    file_name = "example.txt"
+    text = "Hello World!"
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=text)     
+    
     st.header("Cornea rare disease finder")
 
     yes_no_unknown = {"I'm not sure!": 'unknown', 'Yes': 'Yes', 'No': 'No'}
